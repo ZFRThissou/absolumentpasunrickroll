594fa246a791348623765e0c58472eb2552cb5e3
ZFRThissou <ZFRThissou@gmail.com>
2025-07-28 18:11:21 +0200

    Create videos.js

---
+++ js/videos.js
@@ -0,0 +1,14 @@
+const videos = [
+    { titre: "c'est honteux", fichier: "../image/mèmes/vidéos/c'est honteux.mp4" },
+    { titre: "ça va péter", fichier: "../image/mèmes/vidéos/ça va péter .mp4" },
+    { titre: "oh comme c'est bizarre", fichier: "../image/mèmes/vidéos/oh comme cest bizarre.mp4" },
+    { titre: "encore ça fait beaucoup là non", fichier: "../image/mèmes/vidéos/encore ça fait beaucoup là non.mp4" },
+    { titre: "comment ça mon reuf", fichier: "../image/mèmes/vidéos/comment ça mon reuf.mp4" },
+    { titre: "shrek aux toilettes", fichier: "../image/mèmes/vidéos/shrek aux toilettes.mp4" },
+    { titre: "imagination", fichier: "../image/mèmes/vidéos/imagination.mp4" },
+    { titre: "ah c'est marrant", fichier: "../image/mèmes/vidéos/ah cest marrant.mp4" },
+    { titre: "c'est quoi le mot frère", fichier: "../image/mèmes/vidéos/cest quoi le mot frère.mp4" },
+    { titre: "je suis pas venue ici pour souffrir ok", fichier: "../image/mèmes/vidéos/je suis pas venue ici pour souffrir ok.mp4" },
+    { titre: "le long pavé le pavlar", fichier: "../image/mèmes/vidéos/le long pavé le pavlar.mp4" },
+    { titre: "c'est qui a touché a dorian", fichier: "../image/mèmes/vidéos/c'est qui a touché a dorian.mp4" }
+];
