c08331befe2690ba8f9cbfd5f3977b9eaf88ac4f
ZFRThissou <ZFRThissou@gmail.com>
2025-11-25 12:19:43 +0100

    Add files via upload

---
+++ changer extension.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+
+folder = "../image/mèmes3/images"
+
+for file in os.listdir(folder):
+    if file.lower().endswith((".jpg", ".jpeg")):
+
+        input_path = os.path.join(folder, file)
+        img = Image.open(input_path)
+
+        # Convertir en RGB (évite certaines erreurs)
+        img = img.convert("RGB")
+
+        output_path = os.path.join(folder, os.path.splitext(file)[0] + ".png")
+
+        img.save(output_path, "PNG")
+        print(f"Converti : {file} → {os.path.basename(output_path)}")
