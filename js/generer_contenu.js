9d279a3c3b7b1f4014ef7e7fd5dd931e0af4b2d6
ZFRThissou <ZFRThissou@gmail.com>
2025-11-26 17:51:09 +0100

    Update generer_contenu.js

--- js/generer_contenu.js
+++ js/generer_contenu.js
@@ -8,11 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // Détermination du type de page avec la correction pour les URL propres (ex: /vidéos)
     if (path.includes('videos') || path.includes('vidéos')) {
         pageType = 'videoFavorites'; 
+        console.log(`vidéo`);
     } else if (path.includes('/audios')) {
         pageType = 'audioFavorites'; 
+        console.log(`audio`);
     } else if (path.includes('/images')) {
         pageType = 'imageFavorites'; 
+        console.log(`image`);
     } else {
+        console.log(`rien`);
         return;
     }
     
