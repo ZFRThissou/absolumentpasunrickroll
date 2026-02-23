61cb162d2932bd3f997619daf36db0aac83451e0
Absolumentpasunrickroll Corporation <ZFRThissou@gmail.com>
2026-02-23 21:53:44 +0100

    Update get-all-likes.js

--- netlify/functions/get-all-likes.js
+++ netlify/functions/get-all-likes.js
@@ -3,12 +3,14 @@ const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
 
 exports.handler = async () => {
     try {
-        const stats = await sql`SELECT id_meme, likes FROM stats_memes`;
+        // On récupère tout pour pouvoir trier côté client
+        const stats = await sql`SELECT id_meme, likes, duree, date_ajout FROM stats_memes`;
         return {
             statusCode: 200,
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(stats),
         };
     } catch (error) {
-        return { statusCode: 500, body: error.message };
+        return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
     }
 };
