36a1676799fbaa68662a77f7ab8b13f84366c6cf
Absolumentpasunrickroll Corporation <ZFRThissou@gmail.com>
2026-04-24 12:01:53 +0200

    Update generer_contenu.js

--- js/generer_contenu.js
+++ js/generer_contenu.js
@@ -255,12 +255,14 @@ document.addEventListener('DOMContentLoaded', function() {
         closeBtn.onclick = () => {
             modal.style.display = 'none';
             container.innerHTML = ''; // Stop la vidéo
+            audio.pause();
             document.body.style.overflow = '';
         };
         window.onclick = (event) => {
             if (event.target == modal) {
                 modal.style.display = 'none';
                 container.innerHTML = '';
+                audio.pause();
                 document.body.style.overflow = '';
             }
         };
@@ -270,6 +272,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (event.key === "Escape") {
                 modal.style.display = 'none';
                 container.innerHTML = '';
+                audio.pause();
                 document.body.style.overflow = ''; // Réactive le scroll
                 window.removeEventListener('keydown', escHandler); // Nettoie l'écouteur
             }
