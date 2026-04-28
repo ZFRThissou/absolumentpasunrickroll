acf84dc5245cdcb9c0a3f7aac5e1af527c0d9888
Absolumentpasunrickroll Corporation <ZFRThissou@gmail.com>
2026-04-28 16:10:01 +0200

    Update afficher favoris.js

--- js/afficher favoris.js
+++ js/afficher favoris.js
@@ -164,7 +164,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = 'hidden';
         title.textContent = mème.title;
         container.innerHTML = '';
-        desc.textContent = `${mème.desc ? mème.desc : 'La description n'est pas encore disponible.'}`;
+        desc.textContent = `${mème.desc ? mème.desc : "La description n'est pas encore disponible."}`;
         if (mème.type === 'video') {
             const video = document.createElement('video');
             video.src = mediaPath;
